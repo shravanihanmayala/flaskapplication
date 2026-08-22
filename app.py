bd75160de3d370bfff99511ec850e6c0b68345da
Sravani <shravanihanmayala@gmail.com>
2026-08-22 15:11:50 +0530

    Update home route response in app.py
    Change home route response to an empty string.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "sravani running sucessfully"
+    return ""
 if __name__=="__main__":
     app.run(host="0.0.0",port=5000,debug=True)
 
