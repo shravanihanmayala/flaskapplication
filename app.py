1ce76281da1568743d5318d3c5c683bb67395653
Sravani <shravanihanmayala@gmail.com>
2026-08-22 15:10:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "flask running sucessfully"
+    return "sravani running sucessfully"
 if __name__=="__main__":
     app.run(host="0.0.0",port=5000,debug=True)
 
