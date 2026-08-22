025dd472abdbd7de594441e4065572696085f7ec
Sravani <shravanihanmayala@gmail.com>
2026-08-22 15:15:10 +0530

    Modify home route to return HTML response
    Update home route to return HTML content.

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return ""
-if __name__=="__main__":
-    app.run(host="0.0.0",port=5000,debug=True)
+    return """
+    <html>
+    <head>
+        <body>
+            <h1>HELLO RGUKT</h1>
+        </body>
+    </html>"""
+   
+    
+
 
